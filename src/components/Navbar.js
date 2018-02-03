9689d54b96e657055f1cce3a23b45319fe69d791
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-03 17:22:43 +0700

    make navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const navbar = (props) => (
+  <nav className="h3 w-100 border-box bg-wg-red v-mid font-nunito flex justify-between ph3">
+    <a href="#" className="link f4 pt3 wg-broken-white">WartegApp</a>
+    <a href="#"  className="link f4 pt3 wg-broken-white">Log In</a>
+  </nav>
+);
+
+export default navbar;

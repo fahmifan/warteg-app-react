7bcd13bee0cd7fa997ac5e30792cf70ea9bbc255
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-04 17:28:22 +0700

    add margin to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const navbar = (props) => (
   <nav className="h3 w-100 border-box bg-wg-red v-mid font-nunito flex justify-between ph3">
-    <a href="#" className="link f4 pt3 wg-broken-white">WartegApp</a>
-    <a href="#"  className="link f4 pt3 wg-broken-white">Log In</a>
+    <a href="#" className="link f4 mt1 pt3 pb3 wg-broken-white">WartegApp</a>
+    <a href="#"  className="link f4 mt1 pt3 pb3 wg-broken-white">Log In</a>
   </nav>
 );
 

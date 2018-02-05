85e0027dbb2b73a4cf09c1719cb1ebdb7ee9ee8b
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-05 13:21:12 +0700

    change Input. to SearchField.js

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import Auxi from '../hoc/Auxi';
 import searchIcon from '../assets/icon/search-icon.svg';
 
 const input = (props) => {
    return (
       <div className="flex border-box w-80 center ba b--black-20 br2 pv1 pl2 bg-white">
-        <Auxi><img src={searchIcon} className="pt1" alt="search"/></Auxi>
+        <img src={searchIcon} className="pt1" alt="search"/>
         <input 
           type="text"  
           placeholder="Cari kedai" 

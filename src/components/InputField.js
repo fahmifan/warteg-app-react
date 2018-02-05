5cb87ee5248d3e22c62eba1a58651e2464389754
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-05 13:22:29 +0700

    make InputFields comps

---
+++ src/components/InputField.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Auxi from '../hoc/Auxi';
+
+const inputField = (props) => (
+  <div className="flex border-box w-80 center mt3">
+    <div className="w-100 br2 ba b--black-10 bw1">
+    <input 
+      type="text"  
+      placeholder={`${props.placeholder}`} 
+      className="w-100 h2 font-nunito ph2 pv1 wg-black ba-0" 
+      style={{border: "none", outline: "none"}}/>
+    </div>
+  </div>
+);
+
+export default inputField;

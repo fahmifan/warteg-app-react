f5ff4064e67641adf4001b393d888c7245c23e5c
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-05 10:24:13 +0700

    make Footer comps

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const footer = () => (
+  <footer className="tc font-nunito-bold wg-black pv3 mt2" style={{backgroundColor: '#eee'}} >
+    <span>WartegApp &copy; 2018</span> <br/>
+    <span>Made with <span style={{color: 'red'}}>&#10084;</span> </span>
+  </footer>
+);
+
+export default footer;

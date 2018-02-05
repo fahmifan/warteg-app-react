1ecf2b957aa24b4b3b2c88ec31388e0b3043bcee
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-05 13:34:02 +0700

    fix height & add Footer

--- src/layout/SignIn.js
+++ src/layout/SignIn.js
@@ -4,6 +4,7 @@ import Auxi from '../hoc/Auxi';
 import NavBar from '../components/Navbar';
 import InputField from '../components/InputField';
 import Button from '../components/Button';
+import Footer from '../components/Footer';
 
 class SignIn extends Component {
   render() {
@@ -12,13 +13,14 @@ class SignIn extends Component {
         <NavBar isSignedPage={true} />
         <header className="f1 tc font-nunito wg-black pt4"> 
           Masuk</header>
-        <main className="center">
+        <main className="center vh-75">
           <InputField placeholder="Email" />
           <InputField placeholder="Kata Sandi" />
           <Button>Masuk</Button>
           <p className="tc ma0 pt3 font-nunito wg-black f5">
             Belum mendaftar? <span className="link dim wg-blue">Daftar</span> Sekarang</p>
         </main>
+        <Footer />
       </Auxi>
     );
   }
--- src/layout/SingUp.js
+++ src/layout/SingUp.js
@@ -4,6 +4,7 @@ import Auxi from '../hoc/Auxi';
 import NavBar from '../components/Navbar';
 import InputField from '../components/InputField';
 import Button from '../components/Button';
+import Footer from '../components/Footer';
 
 class SignUp extends Component {
   render() {
@@ -12,7 +13,7 @@ class SignUp extends Component {
         <NavBar isSignedPage={true} />
         <header className="f1 tc font-nunito wg-black pt4"> 
           Daftar</header>
-        <main className="center">
+        <main className="center vh-75">
           <InputField placeholder="Nama Lengkap" />
           <InputField placeholder="Email" />
           <InputField placeholder="Kata Sandi" />
@@ -20,6 +21,7 @@ class SignUp extends Component {
           <p className="tc ma0 pt3 font-nunito wg-black f5 no">
             Sudah punya akun ? Yuk <a href="#" className="link dim wg-blue">Masuk</a></p>
         </main>
+        <Footer />
       </Auxi>
     );
   }

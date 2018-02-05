f54d687c5cffed615c3557e5e18e5ba64ea7c934
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-05 13:21:51 +0700

    add logic to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import backArrow from '../assets/icon/back-arrow-button.svg';
+
 const navbar = (props) => (
-  <nav className="h3 w-100 border-box bg-wg-red v-mid font-nunito flex justify-between ph3">
-    <a href="#" className="link f4 mt1 pt3 pb3 wg-broken-white">WartegApp</a>
-    <a href="#"  className="link f4 mt1 pt3 pb3 wg-broken-white">Log In</a>
+  <nav className="h3 w-100 border-box bg-wg-red v-mid flex justify-between ph3 shadow-5">
+    <a href="#" className="link f4 mt1 pt3 pb3 font-nunito-bold wg-broken-white">WartegApp</a>
+    { 
+      props.isSignedPage ? <img src={backArrow} className="pointer" alt="Back" onClick={props.clicked} /> 
+      : <a onClick={props.clicked} href="#"  className="link f4 font-nunito-bold mt1 pt3 pb3 wg-broken-white">Log In</a>
+    }
   </nav>
 );
 

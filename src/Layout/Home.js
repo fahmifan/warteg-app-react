2c3ef32def52a646a28d90724c6234ee4c0860aa
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-05 13:25:21 +0700

    add state and loginClicked method

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Navbar from '../components/Navbar';
-import Input from '../components/Input';
+import SearchField from '../components/SearchField';
 import StreetFoodCard from '../components/StreetFoodCard';
 import FoodCard from '../components/FoodCard';
 import Footer from '../components/Footer';
@@ -9,13 +9,22 @@ import Footer from '../components/Footer';
 import img from '../assets/pics/nasi-goreng-kambing.jpg';
 
 export default class Home extends Component {
+
+  state = {
+    isSignedPage: false
+  }
+
+  loginClicked = () => {
+    this.setState({isSignedPage: !this.state.isSignedPage})
+  }
+
   render() {
     return (
       <div className="h-100 bg-wg-white">
-        <Navbar />
+        <Navbar isSignedPage={this.state.isSignedPage} clicked={this.loginClicked} />
         <p className="f2 mt4 mb3 tc font-nunito-bold wg-black">Jatinangor</p>
         <div className="mb4">
-          <Input />
+          <SearchField />
         </div>
         <div className="flex flex-wrap justify-center">
           <FoodCard img={img} />

91229d0930782ac9db6dfc65c56d978052a7b58b
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-06 21:44:32 +0700

    add method & passing down props

--- src/layout/HomeSignedIn.js
+++ src/layout/HomeSignedIn.js
@@ -8,16 +8,25 @@ import Footer from '../components/Footer';
 import img from '../assets/pics/nasi-goreng-kambing.jpg';
 
 class HomeSignedIn extends Component {
+  
+  foodCardClicked = (path) => {
+    this.props.history.push('/details' + `/${path}`)
+  }
+
+  dpClicked = () => {
+    console.log('dp clicked should be accordion')
+  }
+
   render() {
     return (
       <Auxi>
-        <NavbarSignedIn />
+        <NavbarSignedIn isBack={false} homeClicked={'/home'} dpClicked={this.dpClicked} />
         <main className="h-100 flex flex-wrap justify-center">
-          <FoodCard img={img} />
-          <FoodCard img={img} />
-          <FoodCard img={img} />
-          <FoodCard img={img} />
-          <FoodCard img={img} />
+          <FoodCard img={img} clicked={() => this.foodCardClicked('hipotesa')} />
+          <FoodCard img={img} clicked={() => this.foodCardClicked('rahayu')} />
+          <FoodCard img={img} clicked={() => this.foodCardClicked('family')} />
+          <FoodCard img={img} clicked={() => this.foodCardClicked('dechick')} />
+          <FoodCard img={img} clicked={() => this.foodCardClicked('warsub')} />
         </main>
         <Footer />
       </Auxi>

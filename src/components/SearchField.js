b10de2ae4505d67d1ab3a00f17afc747d2f0f95a
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-06 21:43:04 +0700

    add onChange

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import searchIcon from '../assets/icon/search-icon.svg';
 
-const input = (props) => {
+const searchField = (props) => {
    return (
       <div className="flex border-box w-80 center ba b--black-20 br2 pv1 pl2 bg-white">
         <img src={searchIcon} className="pt1" alt="search"/>
@@ -10,9 +10,10 @@ const input = (props) => {
           type="text"  
           placeholder="Cari kedai" 
           className="w-100 pr3 mr1 font-nunito wg-black ba-0" 
-          style={{border: "none", outline: "none"}}/>
+          style={{border: "none", outline: "none"}}
+          onChange={props.typed}/>
       </div>
     )
 }
 
-export default input;
+export default searchField;

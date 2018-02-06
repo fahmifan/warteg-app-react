67f1349bc83740246ce96260bf563eaf648dee97
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-06 08:57:26 +0700

    add indentation

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 const Image = styled.img`
-background-image: url(${props => props.img});
-background-size: cover;
-background-position: center;
-height: 100%;
-width: 100%;
-border-radius: 5px;
+  background-image: url(${props => props.img});
+  background-size: cover;
+  background-position: center;
+  height: 100%;
+  width: 100%;
+  border-radius: 5px;
 `;
 
 export default Image

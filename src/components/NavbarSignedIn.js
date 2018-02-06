4d6ec35dca03cec2f1f386eb46bc1fb09e20f4d0
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-06 21:41:39 +0700

    change `a tag` to `Link`

--- src/components/NavbarSignedIn.js
+++ src/components/NavbarSignedIn.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import noDisplayPic from '../assets/icon/noDisplayPic.svg';
 
 const navbar = (props) => (
   <nav className="h3 w-100 border-box bg-wg-red v-mid flex justify-between ph3 shadow-5">
-    <a href="#" className="link f4 mt1 pt3 pb3 font-nunito-bold wg-broken-white">WartegApp</a>
-     <img src={noDisplayPic} className="pointer" alt="Back" onClick={props.clicked} /> 
+    <Link to={props.homeClicked} className="link f4 mt1 pt3 pb3 font-nunito-bold wg-broken-white">WartegApp</Link>
+     <img src={noDisplayPic} className="pointer" alt="Back" onClick={props.dpClicked} /> 
   </nav>
 );
 

e1ca9a8b51d9b59f9248b947beb1a17e93786f02
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-06 21:43:29 +0700

    add pointer when hovered

--- src/components/StreetFoodCard.js
+++ src/components/StreetFoodCard.js
@@ -8,7 +8,7 @@ const streetFoodCard = (props) => {
   const textShadow = {textShadow: '1px 1px rgba(0,0,0,0.2)'};
 
   return (
-    <div className="relative br4 h4 w5 border-box ma2 shadow-3">
+    <div className="relative br4 h4 w5 border-box ma2 shadow-3 pointer dim">
       <TextWrapper>
         <span 
           className="font-nunito-bold ma0 white f3 v-mid tc lh-copy"

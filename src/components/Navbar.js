c5546513a6ae6b3811f2087b180f4b4811669aa5
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-06 21:41:08 +0700

    fix `Link to`

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,13 @@ import backArrow from '../assets/icon/back-arrow-button.svg';
 
 const navbar = (props) => (
   <nav className="h3 w-100 border-box bg-wg-red v-mid flex justify-between ph3 shadow-5">
-    <Link to="/" className="link f4 mt1 pt3 pb3 font-nunito-bold wg-broken-white">WartegApp</Link>
+    <Link to={props.link} className="link f4 mt1 pt3 pb3 font-nunito-bold wg-broken-white">WartegApp</Link>
     { 
-      props.isSignedPage ? <img src={backArrow} className="pointer" alt="Back" onClick={props.clicked} />
-      : <Link onClick={props.clicked} to="/signin"  className="link f4 font-nunito-bold mt1 pt3 pb3 wg-broken-white">Log In</Link>
+      props.isBack ? <img src={backArrow} className="pointer" alt="Back" onClick={props.clicked} />
+      : <Link 
+          // onClick={props.clicked} 
+          className="link f4 font-nunito-bold mt1 pt3 pb3 wg-broken-white"
+          to="/signin">Log In</Link>
     }
   </nav>
 );

84e34c10347270755c45d6789721579ca91558ef
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-06 21:42:38 +0700

    add onClick

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,8 @@ const button = (props) => (
   <div className="w-80 center h2 mt3 br2">
     <button 
       type="button"
-      className="font-nunito-bold w-100 h2 f3 ba b--black-10 br2 bg-wg-yellow wg-white dim pointer">
+      className="font-nunito-bold w-100 h2 f3 ba b--black-10 br2 bg-wg-yellow wg-white dim pointer"
+      onClick={props.clicked}>
       {props.children}
     </button>
   </div>

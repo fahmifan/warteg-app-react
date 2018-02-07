402fbb478976002b71aca15d1dbbf08798bd0c0a
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-07 15:30:48 +0700

    make actionDetails

---
+++ src/layout/Details/actionDetails.js
@@ -0,0 +1,38 @@
+import * as actionTypes from '../../store/actions/actionTypes';
+import axios from 'axios';
+
+const server = "http://localhost:3001/restos";
+
+export const fetchRestoStart = () => {
+  return {
+    type: actionTypes.DETAILS_FETCH_RESTO_START
+  }
+} 
+
+export const fetchRestoSuccess = (resto) => {
+  return {
+    type: actionTypes.DETAILS_FETCH_RESTO_SUCCESS,
+    resto: resto
+  }
+}
+
+export const fetchRestoFail = (error) => {
+  return {
+    type: actionTypes.DETAILS_FETCH_RESTO_FAIL,
+    error: error
+  }
+}
+
+export const fetchResto = (id) => {
+  return dispatch => {
+    axios.get(server + `/${id}`, {"headers": {"Access-Control-Allow-Origin": "*"}})
+      .then(res => {
+        dispatch(fetchRestoSuccess(res.data));
+        console.log('Fetch Resto Success', res.data)
+      })
+      .catch(error => {
+        dispatch(fetchRestoFail(error))
+        console.log('Fetch Resto Error', error)
+      })
+  }
+}

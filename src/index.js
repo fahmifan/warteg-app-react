e15ae01f41c5bcf33984a8039001d5ccf37a7445
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-07 12:39:02 +0700

    Apply redux & thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import homepageReducer from './layout/Homepage/reducerHomepage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers =  process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose; 
+
+const rootReducer = combineReducers({
+  homepage: homepageReducer
+});
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+const app = <Provider store={store}><App /></Provider>
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();

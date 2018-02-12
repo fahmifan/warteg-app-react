598904cda1da962809f53a63610b60bf18db5416
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-12 11:20:03 +0700

    refine button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const button = (props) => (
-  <div className="w-80 center h2 mt3 br2">
-    <button 
-      type="button"
-      className="font-nunito-bold w-100 h2 f3 ba b--black-10 br2 bg-wg-yellow wg-white dim pointer"
-      onClick={props.clicked}>
-      {props.children}
-    </button>
-  </div>
+  <button 
+    type={props.type}
+    className="font-nunito-bold w-100 h2 f3 mt2 ba b--black-10 br2 bg-wg-yellow wg-white dim pointer"
+    onClick={props.clicked}>
+    {props.children}
+  </button>
 );
 
 

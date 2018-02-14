0201757eec54e81fb12b8424a6d53472579a12be
fahmi-irfan <fahmiirfan909@gmail.com>
2018-02-14 13:57:50 +0700

    add authReducer

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,39 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initState = {
+  id: null,
+  token: null,
+  email: null,
+  isLogedIn: false,
+  loading: false,
+  error: null
+}
+
+const reducer = (state = initState, action) => {
+  switch(action.type) {
+    case actionTypes.LOGIN_START:
+      return {
+        ...state,
+        loading: true
+      }
+    case actionTypes.LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        token: action.token,
+        email: action.email,
+        id: action.id,
+        isLogedIn: true
+      }
+    case actionTypes.LOGIN_FAILED:
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      }
+    default: 
+      return state;
+  }
+}
+
+export default reducer;
